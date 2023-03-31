75caf144da5651756734961aa2679a25edc9d9ef
abijith <abijith.mullassery.234@gmail.com>
2023-03-31 12:51:56 +0530

    shopping_cart

---
+++ shoppingcart/shopping_cart/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from . import views
+from django.urls import path, include
+
+app_name='shop'
+
+
+urlpatterns = [
+    path("",views.allProdCat,name='allProdCat'),
+    path("<slug:c_slug>/",views.allProdCat,name='Products_by_Category'),
+    path("<slug:c_slug>/<slug:product_slug>/",views.proDetail,name='proCatDetail'),
+]
